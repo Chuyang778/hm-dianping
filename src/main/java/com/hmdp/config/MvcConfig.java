e1f3d098db4b8da7c89177d3454f422cd16e1eed
Xcy <3062995781@qq.com>
2022-12-19 23:10:34 +0800

    Second type

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,29 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author ChuYang
+ * @version 1.0
+ */
+@Slf4j
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/shop/**",
+                        "/voucher/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/blog/hot",
+                        "/user/code",
+                        "/user/login"
+                );
+    }
+}
--- src/main/java/com/hmdp/config/MybatisConfig.java
+++ src/main/java/com/hmdp/config/MybatisConfig.java
@@ -3,10 +3,12 @@ package com.hmdp.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class MybatisConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
