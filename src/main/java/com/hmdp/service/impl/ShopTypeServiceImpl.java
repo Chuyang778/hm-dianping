5f752e38d9067a8821388052d4d5e317c09fce40
Xcy <3062995781@qq.com>
2022-12-21 16:27:10 +0800

    fourth type

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,14 +1,22 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -17,4 +25,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryTypeList() {
+        String key = RedisConstants.CACHE_TYPE_KEY;
+        //从缓存中查询数据
+        String typeJson = stringRedisTemplate.opsForValue().get(key);
+        //判断是否存在
+        if (StrUtil.isNotBlank(typeJson)) {
+            List<ShopType> typeList = JSONUtil.toList(typeJson, ShopType.class);
+            return Result.ok(typeList);
+        }
+        //不存在直接查询数据库
+        List<ShopType> shopTypeList = query().orderByAsc("sort").list();
+        if (shopTypeList == null) {
+            return Result.fail("分类不存在");
+        }
+        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shopTypeList));
+
+        return Result.ok(shopTypeList);
+
+    }
 }
