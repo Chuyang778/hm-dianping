008cc9a6571d80a430324d02be2a572bb28c0fb7
Xcy <3062995781@qq.com>
2023-01-04 11:17:17 +0800

    ninth type

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,46 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author ChuYang
+ * @version 1.0
+ */
+
+@Component
+public class RedisIdWorker {
+    /**
+     * 开始时间戳
+     * @param keyPrefix
+     * @return
+     */
+    private static final long BEGIN_TIMESTAMP = 1640995200L;
+    /*
+    序列号
+     */
+    private static final int COUNT_BITS = 32;
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public long nextId(String keyPrefix) {
+        // 1.生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = nowSecond - BEGIN_TIMESTAMP;
+
+        //2.生成序列号
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+        //3.拼接并返回
+        return timestamp << COUNT_BITS | count;
+    }
+
+}
